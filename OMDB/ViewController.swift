b55b6e11eb4bcfcfc052e63eb074b6aa38fe27c4
Death Code <deathcode@deaths-macbook-pro.local>
2017-03-12 21:29:49 +0530

    Initial Commit

---
+++ OMDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OMDB
+//
+//  Created by Death Code on 12/03/17.
+//  Copyright © 2017 Death Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
